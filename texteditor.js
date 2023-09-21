23d100c723f34d68fdb6e04c78da27577bf5a68b
13Elsa <125810161+13Elsa@users.noreply.github.com>
2023-09-21 22:19:13 +0530

    Update texteditor.js

--- texteditor.js
+++ texteditor.js
@@ -1,1 +1,11 @@
+// Get the bold button element
+const boldButton = document.getElementById("boldButton");
 
+// Add a listener for the "click" event on the bold button
+boldButton.addEventListener("click", function() {
+  // Get the current selection in the text editor
+  const selection = editor.getSelection();
+
+  // Bold the selected text
+  selection.execCommand("bold");
+});
